48ea7d6790fc7702ffb67a938f824df1ad6c4cad
Rod' Rodriguez <Rod.R.Code@gmail.com>
2020-08-05 14:11:05 -0500

    Update Ship.cs
    corrected ship class

--- SGClasses/Ship.cs
+++ SGClasses/Ship.cs
@@ -6,24 +6,24 @@ namespace SGClasses
     {
         // TODO Type of ship, Name, Total Capacity, Speed, Fuel Level, Offence, Defence, Shield, Cargo hold?
         
-        public readonly int ShipType;
-        public readonly string ShipName;
-        public readonly double TotalCapacity;
-        public readonly double Speed;
-        public readonly double FuelLevel;
-        public readonly double Offence;
-        public readonly double Defense;
-        public readonly double Shield;
+        public readonly int shipType;
+        public readonly string shipName;
+        public readonly double totalCapacity;
+        public readonly double speed;
+        public readonly double fuelLevel;
+        public readonly double offence;
+        public readonly double defense;
+        public readonly double shield;
         public Ship (int shipType, string shipName, double totalCapacity, double speed, double fuelLevel, double offence, double defense, double shield)
         {
-        ShipType = shiptype;
-        ShipName = shipname;
-        TotalCapacity = totalCapacity;
-        Speed = speed;
-        FuelLevel = fuelLevel;
-        Offence = offence;
-        Defense = defense;
-        Shield = shield;
+        this.shipType = shipType;
+        this.shipName = shipName;
+        this.totalCapacity = totalCapacity;
+        this.speed = speed;
+        this.fuelLevel = fuelLevel;
+        this.offence = offence;
+        this.defense = defense;
+        this.shield = shield;
         }
 
     }

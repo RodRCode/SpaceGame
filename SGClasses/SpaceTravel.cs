24a5b2f74981bf58912cfa29fd76ad43218ff291
Rod' Rodriguez <Rod.R.Code@gmail.com>
2020-08-06 11:04:54 -0500

    add the finished distance calculation and the warp speed calculations, returning the time and fuel

--- SGClasses/SpaceTravel.cs
+++ SGClasses/SpaceTravel.cs
@@ -6,36 +6,22 @@ namespace SGClasses
 {
     public class SpaceTravel
     {
-        //input current location coordinates and destination coordinates. Also maybe input the names of origin and destination 
-        // after calculating distance traveled, calculate time taken and fuel consumed. we need to figure out a formula for speed consuption.
-
-        public static double DistanceCalculation (Location location1, Location location2)
+        //receives two locations, then calculates the distance between them
+        public static double DistanceCalculation(Location location1, Location location2)
         {
-            double distance = 0.0;
             int x = location1.x - location2.x;
             int y = location1.y - location2.y;
-
-            x = 4;
-            y = 5;
-
-            distance = Math.Sqrt((x * x) + (y * y));
-
-            Console.WriteLine(distance);
-
+            double distance = Math.Sqrt((x * x) + (y * y));
             return distance;
         }
 
-        public static double WarpSpeedCalcuation (double distance, int warpSpeed)
+        // Recieves the distance, and the warp speed you want to use, and returns the time and fuel needed to travel
+        public static (double, double) WarpSpeedCalcuation(double distance, int warpSpeed)
         {
-            double time = 0.0;
-            double fuel = 0.0;
-            int warpSpeed = 5.0;
-            double distance = 20;
-
-            time = ((warpSpeed) ^ (10 * 3)) + ((10 - warpSpeed) ^ ((-11) / 3));
-            Console.WriteLine(time);
+            double warpFactor = ((Math.Pow(warpSpeed, (10 / 3)) + Math.Pow((10 - warpSpeed), (-11 / 3))));
+            double fuel = distance * warpFactor;
+            double time = 1 / warpFactor;
+            return (time, fuel);
         }
-
-
     }
 }

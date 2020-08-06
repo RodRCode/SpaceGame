83b01665bded8538ff730c44405f6dffd4e4f96a
Jay Rudometkin <jayrudometkin@gmail.com>
2020-08-06 09:39:28 -0400

    Added Funtionality to call Widget Class

--- SpaceGameTUI/Program.cs
+++ SpaceGameTUI/Program.cs
@@ -55,6 +55,42 @@ namespace SpaceGameTUI
 
 
 
+          
+
+            Console.SetWindowSize(140, 40);
+            var root = new RootWindow();
+
+            var banner = new Dialog(root) { Text = "SPACE HAWKER", Width = 136, Height = 7, Top = 2, Left = 2, Border = BorderStyle.Thick };
+            new Label(banner) { Text = "Name: " + player.Name, Top = 1, Left = 4 };
+            new Label(banner) { Text = "AGE: " + player.Age, Top = 2, Left = 4 };
+            new Label(banner) { Text = "Bank Account" + player.Money, Top = 3, Left = 4 };
+
+
+            //var button = new Button(dialog) { Text = "Play", Top = 4, Left = 6 };
+            //var button2 = new Button(dialog) { Text = "Click", Top = 4, Left = 18 };
+            //var list = new ListBox(dialog) { Top = 10, Left = 4, Width = 32, Height = 6, Border = BorderStyle.Thin };
+            //var line = new VerticalLine(dialog) { Top = 4, Left = 40, Width = 1, Height = 6, Border = BorderStyle.Thick };
+            //var dialog2 = new Dialog(root) { Text = "ooooh", Width = 32, Height = 5, Top = 6, Left = 6, Border = BorderStyle.Thick, Visible = false };
+            //var button3 = new Button(dialog2) { Text = "Bye!", Width = 8, Height = 3, Top = 1, Left = 1 };
+
+            //button3.Clicked += (s, e) => { dialog2.Hide(); dialog.Show(); };
+            //button2.Clicked += (s, e) => { dialog.Hide(); dialog2.Show(); };
+
+            //for (var i = 0; i < 25; i++ )
+            //{
+            //    list.Items.Add("Item " + i.ToString());
+            //}
+
+            //button.Clicked += button_Clicked;
+
+            root.Run();
+
+
         }
+        static void button_Clicked(object sender, EventArgs e)
+        {
+            (sender as Button).RootWindow.Detach();
+        }
+
     }
 }

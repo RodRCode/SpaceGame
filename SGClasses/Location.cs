03b9a2a0ba7f536b42e21076b131bccae07c9c9c
Jay Rudometkin <jayrudometkin@gmail.com>
2020-08-06 15:43:56 -0400

    Revert "updates to clear out commented out code and clearnup"
    This reverts commit 7aa6557bc3b1656c5f55f2a8b72df3db3f1a0e1b.

--- SGClasses/Location.cs
+++ SGClasses/Location.cs
@@ -14,5 +14,37 @@ namespace SGClasses
             this.x = x;
             this.x = y;
         }
+
+    //    public override string ToString()
+    //    {
+    //        return X + "," + Y;
+    //    }
+    //
+    //    public override bool Equals(object obj)
+    //    {
+    //        if (!(obj is Location))
+    //        {
+    //            return false;
+    //        }
+    //
+    //        Location that = obj as Location;
+    //
+    //        return this.X == that.X && this.Y == that.Y;
+    //    }
+    //
+    //    public override int GetHashCode()
+    //    {
+    //        return X.GetHashCode() * 31 + Y.GetHashCode();
+    //    }
+    //
+    //    public int DistanceTo(int x, int y)
+    //    {
+    //        return (int)Math.Sqrt(Math.Pow(X - x, 2) + Math.Pow(Y - y, 2));
+    //    }
+    //
+    //    public int DistanceTo(Location point)
+    //    {
+    //        return DistanceTo(point.X, point.Y);
+    //    }
     }
 }

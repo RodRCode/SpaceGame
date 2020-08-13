494308a2a013173240f1953bf51e93018d2a18ea
Rod' Rodriguez <Rod.R.Code@gmail.com>
2020-08-13 09:50:10 -0500

    adjust fuel cost

--- code/SpaceGameTUI/Program.cs
+++ code/SpaceGameTUI/Program.cs
@@ -173,8 +173,8 @@ namespace SpaceGameTUI
                         {
                             dialogList.Items.Clear();
                             dialogList.Items.Add("You bought some " + currentItemName);
-                            ship.fuelLevel += 100; ;
-                            player.Money--;
+                            ship.fuelLevel += 250; ;
+                            player.Money -= 10;
                         }
                         else
                         {
